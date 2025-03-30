b899a6748cb40b739572976d7fe68cd170867eda
Geovannisouza23 <151410949+Geovannisouza23@users.noreply.github.com>
2025-03-30 06:02:48 -0300

    Add files via upload
    construir ProductProcessor responsável por processar o arquivo CSV que é enviado via upload, fazendo a leitura e filtragem dos dados.

--- src/server/index.php
+++ src/server/index.php
@@ -1,4 +1,15 @@
 <?php
+// Definir cabeçalhos CORS
+header("Access-Control-Allow-Origin: *"); // Permitir todas as origens (pode restringir para um domínio específico)
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Métodos permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Cabeçalhos permitidos
+
+// Se for uma requisição OPTIONS, responder rapidamente sem processamento adicional
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 class ProductProcessor
 {
     private $separator;
